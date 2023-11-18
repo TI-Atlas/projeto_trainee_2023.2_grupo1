7009c05fd7be9dbfe5c1c4597290fa8cb1b6efd5
PedroAugustoGodoy <d2021005227@unifei.edu.br>
2023-11-17 23:45:33 -0300

    segundo commit

--- src/server.ts
+++ src/server.ts
@@ -2,11 +2,15 @@ import "reflect-metadata";
 import express from "express";
 import "./database";
 import { routes } from "./routes";
+import cors from "cors";
 
 const app = express();
 
+app.use(cors());
+
 app.use(express.json());
+
 
 app.use(routes);
 
-app.listen(3000, () => console.log("Server is running!"));
+app.listen(3001, () => console.log("Server is running!"));
